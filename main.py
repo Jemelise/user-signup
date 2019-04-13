f90b394abdf711747bd6c646d1e867816f895714
Jenna <jemelise@gmail.comgit config --global user.name Jennagit commit -m Initial>
2019-04-13 15:33:13 -0500

    welcome page and keep text in boxes

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ app.config['DEBUG'] = True
 
 @app.route("/", methods=["GET", "POST"])
 def user_signup():
-
+    user_name = ""
     error_message = ""
+    email = ""
+
     if request.method == "POST":
+
         user_name = request.form['username']
         pass_word = request.form['password']
         verifypass = request.form['verify']
@@ -17,29 +20,47 @@ def user_signup():
 
         length_username = len(user_name)
         length_password = len(pass_word)
+        length_email = len(email)
 
         if not user_name:
-            error_message = "Incomplete form"
+            error_message = "*Incomplete form, enter username"
 
         if length_username < 3 or length_username > 20:
             error_message = "Invalid username"
 
         if not pass_word:
-            error_message = "Incomplete form"
+            error_message = "*Incomplete form, enter password"
 
         if length_password < 3 or length_password > 20:
             error_message = "Invalid password"    
 
         if not verifypass:
-            error_message = "Incomplete form"     
+            error_message = "*Incomplete form, verify password"     
 
         if pass_word != verifypass:
-            error_message = "Passwords do not match"           
+            error_message = "Passwords do not match"    
 
-    return render_template("sign_in.html", error=error_message)
+        if email:
+            if length_email < 3 or length_email > 20:
+                error_message = "Invalid email"  
+
+            if "@" not in email:
+                error_message = "Invalid email"
+
+            if "." not in email:
+                error_message = "Invalid email"
+
+            if " " in email:
+                error_message = "Invalid email"                          
+        
+        if not error_message:
+            return render_template("welcome.html", user_name=user_name)
+
+    return render_template("sign_in.html", error=error_message, user_name=user_name, email=email)
 
 @app.route("/welcome", methods=["GET", "POST"])
 def welcome():
-    return render_template("welcome.html")
-    
+    user_name = request.form['username']
+    return render_template("welcome.html", user_name=user_name)
+
 app.run()
